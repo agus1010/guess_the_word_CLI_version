ed6dba97921a3c67762d73c248b3cb744e9aed78
agus <agus.aguirre10@gmail.com>
2023-10-10 23:16:27 -0300

    Started new wordle core with refactors

--- wordfuncs/validation.py
+++ wordfuncs/validation.py
@@ -2,6 +2,52 @@ from . import dictionary, utils
 from game.errors import UserWordError
 
 
+# V2 (CORE)
+from api.game.game import *
+from enum import IntEnum
+
+
+
+class WORD_VALIDATION_STATUS(IntEnum):
+    CORRECT = 0
+    WRONG = 1
+    WORD_NOT_IN_DICTIONARY = 11
+    NOT_ENOUGH_CHARS = 20
+    TOO_MANY_CHARS = 21
+
+
+@dataclass
+class WordValidation:
+    detail:list[int]
+    status:WORD_VALIDATION_STATUS
+
+
+class WordValidator:
+    def __init__(self, game_word:str, game_config:GameConfiguration) -> None:
+        self._word = game_word
+        self._config = game_config
+    
+    def validate(self, input_word:str) -> WordValidation:
+        status = WORD_VALIDATION_STATUS.CORRECT
+        config = self._config
+
+        if (input_len := len(input_word)) != config.max_word_length:
+            if input_len < config.max_word_length:
+                status = WORD_VALIDATION_STATUS.NOT_ENOUGH_CHARS
+            else:
+                status = WORD_VALIDATION_STATUS.TOO_MANY_CHARS
+        
+        if not dictionary.word_is_in_dictionary(input_word.strip(), config.accents):
+            status = WORD_VALIDATION_STATUS.WORD_NOT_IN_DICTIONARY
+        
+        detail = None
+        if status < 10:
+            detail = generate_checksum(self._word, input_word)
+            status = WORD_VALIDATION_STATUS.CORRECT if sum(detail) == config.max_word_length else WORD_VALIDATION_STATUS.WRONG
+
+        return WordValidation(detail, status)
+# V2 (CORE)
+
 
 def generate_checksum(game_word:str, user_word:str) -> list[int]:
     """
@@ -31,10 +77,6 @@ def generate_checksum(game_word:str, user_word:str) -> list[int]:
             else:
                 result[i] = 2
     return result
-
-
-def checksum_is_valid(checksum:list[int]) -> bool:
-    return sum(checksum) == 0
 
 
 def word_is_valid(user_word:str, game_word_length:int, accents:bool):
